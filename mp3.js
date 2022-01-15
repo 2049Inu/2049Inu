20008edd692e80b027ec083bd28d916e186718ef
HPOS10I <96672773+2049Inu@users.noreply.github.com>
2022-01-15 16:15:48 -0500

    Added now playing marquee to music page

--- mp3.js
+++ mp3.js
@@ -1,20 +1,19 @@
-function init()
-{
+function init(){
     document.addEventListener('click', function(evt) { if (evt.target.tagName.toLowerCase() == 'li') { play(evt.target); } }, false);
 }
 
-function skip()
-{
+function skip(){
 }
 
-function play(elem)
-{
-    var audio = document.getElementById('audio');
+function play(elem){
+    let audio = document.getElementById('audio');
+    let songTitle = document.getElementById('now-playing');
+    songTitle.innerHTML = elem.textContent;
     audio.src = './mp3/' + elem.textContent + '.mp3';
     audio.play();
     elem.className = 'playing';
-    skip = function()
-    {
+
+    skip = function(){
             elem.className = '';
             if (elem.nextElementSibling)
             {
